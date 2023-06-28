363cd2d538f8fdbfb9ddd470c09dae80d1bded85
Vivi <vachetvirginie@gmail.com>
2023-06-28 13:03:42 +0200

    feat: add vuex and playlistName state

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+
+const store = createStore({
+    plugins: [createPersistedState()],
+    state() {
+        return {
+            playlistName: '',
+        };
+    },
+    mutations: {
+        setPlaylistName(state, newName) {
+            state.playlistName = newName;
+        },
+    },
+    actions: {
+        updatePlaylistName({ commit }, newName) {
+            commit('setPlaylistName', newName);
+        },
+    },
+    getters: {
+        getPlaylistName(state) {
+            return state.playlistName;
+        },
+    },
+});
+
+export default store;
