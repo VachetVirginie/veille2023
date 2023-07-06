396f11f503da52164cd846b96c1ce610d589773e
Vivi <vachetvirginie@gmail.com>
2023-07-06 15:47:56 +0200

    feat: add vuetify and add style on list of playlist

--- src/main.js
+++ src/main.js
@@ -6,8 +6,21 @@ import "./assets/index.css";
 import { YoutubeVue3 } from "youtube-vue3";
 import router from "./router/router";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.use(vuetify);
 app.component("YoutubeVue3", YoutubeVue3).mount("#app");
 
